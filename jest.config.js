7ee78caaf97f36563a34f2595370168dd642b818
Emmanuel Abraham <sundayemmanuelabraham@gmail.com>
2024-03-21 14:20:17 +0100

    added test

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+// /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */ This is depricated
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [ "/dist/" ]
+};
